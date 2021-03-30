941bee1f107c746ff3cd03d508d6e38cba85888c
Kibin <jacobyoo00@hotmail.com>
2021-03-30 01:11:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,5 @@
 def position_taken?(board, index)
   # binding.pry
 it 'returns false if the board has an empty string " " in the desired index' do
-  board[0] == " "
   expect(position_taken?(board, index)).to be(false)
 end
